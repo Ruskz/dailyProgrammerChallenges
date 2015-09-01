161f5c04b45de0ab7506e4184af6103df8bc6d54
Austin <amelville94@gmail.com>
2015-09-01 00:16:29 -0400

    Added a second test case and began coding the challenge

--- JSON_Treasure_Hunt/JSON_Treasure_Hunt.py
+++ JSON_Treasure_Hunt/JSON_Treasure_Hunt.py
@@ -1,29 +1,20 @@
 import json
 
-json_object1 = {"name": "William Shakespeare", "wife": {"birthYear": 1555, "deathYear": 
-"Fun fact, she's a vampire", "name": "Anne Hathaway", "dead": False}, 
-"favoriteWebsites": ["dailysonneter", "dailyprogrammer", 
-"vine (he's way into 6-second cat videos)"], "dead": True, "birthYear": 1564, 
-"facebookProfile": null, "selectedWorks": [{"written": 1606, "name": 
-"The Tragedy of Macbeth", "isItAwesome": True}, {"written": 1608, "name": 
-"Coriolanus", "isItAwesome": "It's alright, but kinda fascist-y"}], "deathYear":
- 1616}
+json_obj1 = json.load(open("text1.txt"))
+json_obj2 = json.load(open("text2.txt"))
 
-json_object2 = {"dlpgcack": false, "indwqahe": null, "caki": {"vvczskh": null, "tczqyzn": 
-false, "qymizftua": "jfx", "cyd": {"qembsejm": [null, "dailyprogrammer", null], 
-"qtcgujuki": 79, "ptlwe": "lrvogzcpw", "jivdwnqi": null, "nzjlfax": "xaiuf", 
-"cqajfbn": true}, "kbttv": "dapsvkdnxm", "gcfv": 43.25503357696589}, "cfqnknrm": 
-null, "dtqx": "psuyc", "zkhreog": [null, {"txrhgu": false, "qkhe": false, 
-"oqlzgmtmx": "xndcy", "khuwjmktox": 48, "yoe": true, "xode": "hzxfgvw", 
-"cgsciipn": 20.075297532268902}, "hducqtvon", false, [null, 76.8463226047357, 
-"qctvnvo", null], [null, {"nlp": false, "xebvtnvwbb": null, "uhfikxc": null, 
-"eekejwjbe": false, "jmrkaqky": null, "oeyystp": false}, [null, 10, "nyzfhaps", 
-71, null], 40, null, 13.737832677566875], [true, 80, 20, {"weynlgnfro":
-40.25989193717965, "ggsirrt": 17, "ztvbcpsba": 12, "mljfh": false, "lihndukg": 
-"bzebyljg", "pllpche": null}, null, [true, false, 52.532666161803895, "mkmqrhg",
- "kgdqstfn", null, "szse"], null, {"qkhfufrgac": "vpmiicarn", "hguztz": 
- "ocbmzpzon", "wprnlua": null}], {"drnj": [null, false], "jkjzvjuiw": false, 
- "oupsmgjd": false, "kcwjy": null}]}
+#print(json.dumps(json_obj1, indent=1))
+#print(json_obj1)
+word = 'dailyprogrammer'
+map = []
 
-print("".join(json_object1))
-#print(json.dumps(json_object1))
+test_obj = {'a':'dailyprogrammer','b':3}
+
+def find_word(obj, word, map):
+    """ This is a recursive function """
+    #return word in obj - searches the keys
+    #return word in obj - searches the values
+
+    
+
+print(find_word(test_obj, 'a'))
